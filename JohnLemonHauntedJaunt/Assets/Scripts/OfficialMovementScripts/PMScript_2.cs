829c73b4f4367ea9d6c560a9a4b8f36276bc3c60
Borkowski, Mark Daniel <71045065+ToxiicMarker69@users.noreply.github.com>
2021-03-23 14:21:02 -0400

    inputLagPeriod added

--- JohnLemonHauntedJaunt/Assets/Scripts/OfficialMovementScripts/PMScript_2.cs
+++ JohnLemonHauntedJaunt/Assets/Scripts/OfficialMovementScripts/PMScript_2.cs
@@ -6,16 +6,32 @@ public class PMScript_2 : MonoBehaviour
 {
     [SerializeField] private Vector2 sensitivity;
     [SerializeField] private Vector2 acceleration;
+    [SerializeField] private float inputLagPeriod;
     private Vector2 velocity;
     private Vector2 rotation; //the current rotation in degrees
+    private Vector2 lastInputEvent;//The last recieved non-zerro input value
+    private float inputLagTimer;//The time since the last recieved non-zero input value
+    
+    
     // Start is called before the first frame update
     private Vector2 GetInput() {
+        //Add to the lag timer
+        inputLagTimer += Time.deltaTime;
         Vector2 input = new Vector2(
             Input.GetAxis("Mouse X"),
             Input.GetAxis("Mouse Y")
         );
-
-        return input;
+        //Sometimes at fast framerates, Unity will not receive input events every frame, which results
+        //in zero values being given above. This can cause shuttering and make it difficult to fine
+        //tune the acceleration setting. To fix this, disregard zero values. If the lag time has passed the
+        //lag period, we can assume that the user is not giving any input, so we actually want to set
+        //the input value to zero at that time.
+        //Thus, save the input value if it is non-zero or the lag timer is met.
+        if((Mathf.Approximately(0, input.x) && Mathf.Approximately(0, input.y)) == false || inputLagTimer >= inputLagPeriod) {
+            lastInputEvent = input;
+            inputLagTimer = 0;
+        } 
+        return lastInputEvent;
     }
         void Start()
     {
